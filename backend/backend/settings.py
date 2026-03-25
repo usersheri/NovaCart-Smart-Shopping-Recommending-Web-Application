84aa4215f4990b70fd34ce469fec59df5b6ce869
usersheri <sherisherin315@gmail.com>
2026-03-25 11:18:16 +0530

    prepare backend for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,7 +127,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 CORS_ALLOW_ALL_ORIGINS = True
 
